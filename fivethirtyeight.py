2d957f6d82d14f7c63b5c23ba445627f01cfbf09
David Morris <dramamoose@gmail.com>
2018-01-19 01:02:22 -0700

    UpdatedResources

--- fivethirtyeight.py
+++ fivethirtyeight.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 
-data = pd.read_csv('https://raw.githubusercontent.com/dramamoose/ElectionModeler/master/resources/538Data.csv')
-print(data)
+data = pd.read_csv('https://raw.githubusercontent.com/dramamoose/ElectionModeler/master/resources/538Data.csv', index_col=0)
+d = data.transpose().to_dict(orient='series')
