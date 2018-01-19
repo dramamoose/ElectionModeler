66ebc6bd73a5bfa60019fc3891392effd9188c50
David Morris <dramamoose@gmail.com>
2018-01-19 00:51:34 -0700

    Added CSV Resources, Started Getting Data

--- EveryPolitician.py
+++ EveryPolitician.py
@@ -2,58 +2,25 @@ from collections import defaultdict
 from everypolitician import EveryPolitician
 
 
-def getcurrentlegislators():
-    import urllib.request, json
+def getlegislators():
+    housecounts = defaultdict(lambda:0)
+    senatecounts = defaultdict(lambda: 0)
+    nonvoting = ['American Samoa', 'District of Columbia', 'Guam', 'Northern Mariana Islands', 'Puerto Rico',
+                 'US Virgin Islands']
     ep = EveryPolitician()
-
     epusa = ep.country('United-States-of-America')
     epussenate = epusa.legislature('Senate')
-    with urllib.request.urlopen(epussenate.popolo_url) as url:
-        senatedata = json.loads(url.read().decode())
-    epUSHouse = epusa.legislature('House')
-    with urllib.request.urlopen(epUSHouse.popolo_url) as url:
-        housedata = json.loads(url.read().decode())
+    epushouse = epusa.legislature('House')
+    housedata = epushouse.latest_legislative_period().csv()
+    senatedata = epussenate.latest_legislative_period().csv()
+    housedata[:] = [entry for entry in housedata if entry.get('end_date') == '' and entry.get('area') not in nonvoting]
+    senatedata[:] = [entry for entry in senatedata if entry.get('end_date') == '']
+    for entry in housedata:
+        housecounts[entry['group']] = housecounts[entry['group']] + 1
+    for entry in senatedata:
+        senatecounts[entry['group']] = senatecounts[entry['group']] + 1
+    housecounts = dict(housecounts)
+    senatecounts = dict(senatecounts)
+    return housedata, senatedata, housecounts, senatecounts
+currenthousemembers, currentsenatemembers, currenthousecounts, currentsenatecounts = getlegislators()
 
-    return housedata, senatedata
-
-
-def getlegislators(legislaturedata):
-    legislatorcount = {}
-    legislatormap = {}
-
-    position_by_area = {}
-    for position in legislaturedata['memberships']:
-        if position['legislative_period_id'] == currentlegislature:
-            position_by_area[position['area_id']] = position
-    person_by_id = {}
-    for person in legislaturedata['persons']:
-        person_by_id[person['id']] = person
-    organization_by_id = {}
-    for organization in legislaturedata['organizations']:
-        organization_by_id[organization['id']] = organization['name']
-        legislatorcount[organization['name']] = 0
-    for state in legislaturedata['areas']:
-        legislatormetadata = {}
-        print(state['name'])
-
-        try:
-            position = position_by_area[state['id']]
-            person = person_by_id[position['person_id']]
-            legislatormetadata['name'] = person['given_name'] + ' ' + person['family_name']
-            legislatormetadata['party'] = organization_by_id[position['on_behalf_of_id']]
-            print(person['given_name'], person['family_name'], organization_by_id[position['on_behalf_of_id']])
-            legislatorcount[organization_by_id[position['on_behalf_of_id']]] = legislatorcount[organization_by_id[
-                position['on_behalf_of_id']]] + 1
-            legislatormap[state['name']] = legislatormetadata
-        except TypeError:
-            print('No Representative')
-        except KeyError:
-            print('No Representative')
-    print(legislatorcount)
-    return legislatorcount, legislatormap
-
-
-housedata, senatedata = getcurrentlegislators()
-currentlegislature = housedata['events'][-1]['id']
-housecount, housemap = getlegislators(housedata)
-senatecount, senatemap = getlegislators(senatedata)
